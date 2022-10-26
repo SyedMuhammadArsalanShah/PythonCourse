925ccc0b09c1ca35a2ef5bf18eaac875a95e0bdf
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-10-26 16:56:39 +0500

    lecture updted

---
+++ Lecture01Variable.py
@@ -0,0 +1,26 @@
+
+#print statement
+
+print("Bismillah")
+
+
+# data types 
+# string , integer, float, boolean
+
+
+
+a="maryam"
+
+b=31
+
+c=5.7
+
+
+
+
+print(type(a))
+print(type(b)) #<class 'int'>
+print(type(c))
+
+
+
---
+++ Lecture02TypecastingAndUserinputs.py
@@ -0,0 +1,30 @@
+
+
+
+a=12
+b=str(a)
+# print(type(b))
+
+
+
+d="70"
+
+c=int(d)
+
+print(type(c))
+
+
+
+
+f=70
+
+e=float(f)
+
+print(type(e))
+
+
+
+studentname=input("enter your name")
+
+
+print(studentname)
