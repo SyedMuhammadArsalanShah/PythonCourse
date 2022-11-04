04aa72066559fad36bb4b05fdf5affea192e9650
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-11-04 16:25:38 +0500

    dictionary

--- Lecture07Dictionary.py
+++ Lecture07Dictionary.py
@@ -1,27 +1,60 @@
 
 
-a = {
+# a = {
 
-    "daniyal": "2001",
-    "age": "123",
-    "hobies": ["playing", "Reading"],
+#     "daniyal": "2001",
+#     "age": "123",
+#     "hobies": ["playing", "Reading"],
+#     "other":{"kashif":"faris", "number":"022"},
+#       12: "maaz"
 
-    12: "maaz"
+# }
+
+
+
+
+# print(a['age'])
+# print(a['other']['number'])
+# print(a['other']['kashif'])
+
+
+
+
+# methods of dictionaries;
+
+
+# print(a.keys())
+# print(a.values())
+# print(a.items())
+# b={"color":"red", "num":"07"}
+# print(a.update(b))
+
+# print(a)
+
+# print(a["other"])
+# print(a.get("other"))
+
+
+
+# print(a["others"])
+# print(a.get("others"))
+
+
+words={
+"saniha":"accident",
+"kitab":"book",
+"qalam":"pen"
 }
+print("check your words ", list(words.keys()))
+
+search=input("enter your word ")
 
 
-
-print(a.keys())
-print(a.values())
+# print(words[search]) #its throw an error while key not there
 
 
-a["age"]=26
+print(words.get(search))
 
-print(a.values())
-
-
-# print(a["hobiess"])
-print(a.get("hobiess"))
 
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,16 @@
-print(a.get("hobiess"))
+a = {
+
+#     "daniyal": "2001",
+#     "age": "123",
+#     "hobies": ["playing", "Reading"],
+#     "other":{"kashif":"faris", "number":"022"},
+#       12: "maaz"
+
+# }
+
+
+
+
+# print(a['age'])
+# print(a['other']['number'])
+# print(a['other']['kashif'])
