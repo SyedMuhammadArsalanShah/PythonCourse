cba34793cb73f2c22d3c98e1b6a3e76a0995712a
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-11-11 22:48:01 +0500

    code updated

---
+++ Lecture09conditionalstatements.py
@@ -0,0 +1,12 @@
+
+
+
+a= int(input("enter your age"))
+if(a<18 ):
+    print("beta choty ho app")
+elif(a==18 or a== 19):
+    print("you can drive but carefully")
+elif(a>18 and a<60):
+    print("you can drive")
+else :
+    print("you are old")
